8b0c9a802a755f85939ae9d192ea09bd28687fe3
TamaKoya <dinosz99@gmail.com>
2024-02-25 16:51:51 +0100

    main.py - Added command for single rolls of dices, and made it available only on test server, due to how Discord global commands synchronization work.
    SingleDice.py - fixed typos and declared functions as static methods.

--- SingleDice.py
+++ SingleDice.py
@@ -1,31 +1,39 @@
-import Random
+import random
+
 
 class SingleRoll:
 
-    def single_d4(self):
-        d4 = Random.randint(1,4)
+    @staticmethod
+    def single_d4():
+        d4 = random.randint(1,4)
         return d4
 
-    def single_d6(self):
-        d6 = Random.randint(1,6)
+    @staticmethod
+    def single_d6():
+        d6 = random.randint(1,6)
         return d6
 
-    def single_d8(self):
-        d8 = Random.randint(1,8)
+    @staticmethod
+    def single_d8():
+        d8 = random.randint(1,8)
         return d8
 
-    def single_10(self):
-        d10 = Random.randint(1,10)
+    @staticmethod
+    def single_d10():
+        d10 = random.randint(1,10)
         return d10
 
-    def single_d12(self):
-        d12 = Random.randint(1,12)
+    @staticmethod
+    def single_d12():
+        d12 = random.randint(1,12)
         return d12
 
-    def single_d20(self):
-        d20 = Random.randint(1,20)
+    @staticmethod
+    def single_d20():
+        d20 = random.randint(1,20)
         return d20
 
-    def single_d100(self):
-        d100 = Random.randint(1,100)
+    @staticmethod
+    def single_d100():
+        d100 = random.randint(1,100)
         return d100
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import datetime
 import os
 import dotenv
-from interactions import listen, slash_command, SlashContext, Client, Intents
-from discordhelp import createEmbed
-
+from SingleDice import SingleRoll
+from interactions import listen, slash_command, SlashContext, Client, Intents, SlashCommandChoice, OptionType, Embed, BrandColors, SlashCommandOption
+import markdown_strings
 
 bot = Client()
 intents = Intents.DEFAULT
@@ -14,9 +15,69 @@ async def on_ready():
     print("Bot ONLINE!")
 
 
-@slash_command(name="test", description="Test command")
-async def test_command_function(ctx: SlashContext):
-    await ctx.send("Test passed!")
-
+@slash_command(name="rollsingle",
+               description="Roll a single dice!",
+               dm_permission=False,
+               options=[
+                   SlashCommandOption(name="dice",
+                                      description="Select a dice",
+                                      required=True,
+                                      type=OptionType.INTEGER,
+                                      choices=[SlashCommandChoice(name="D4", value=1),
+                                               SlashCommandChoice(name="D6", value=2),
+                                               SlashCommandChoice(name="D8", value=3),
+                                               SlashCommandChoice(name="D10", value=4),
+                                               SlashCommandChoice(name="D12", value=5),
+                                               SlashCommandChoice(name="D20", value=6),
+                                               SlashCommandChoice(name="D100", value=7)
+                                               ])
+               ],
+               scopes=[1211283623840325632])
+async def rollsingle_function(ctx: SlashContext, dice: int):
+    match dice:
+        case 1:
+            embed = Embed(title="Single D4 Roll",
+                          description=markdown_strings.header(f"D4: [{SingleRoll.single_d4()}]", 2),
+                          color=BrandColors.WHITE,
+                          timestamp=datetime.datetime.now())
+            await ctx.send(embeds=embed)
+        case 2:
+            embed = Embed(title="Single D6 Roll",
+                          description=markdown_strings.header(f"D6: [{SingleRoll.single_d6()}]", 2),
+                          color=BrandColors.WHITE,
+                          timestamp=datetime.datetime.now())
+            await ctx.send(embeds=embed)
+        case 3:
+            embed = Embed(title="Single D8 Roll",
+                          description=markdown_strings.header(f"D8: [{SingleRoll.single_d8()}]",2),
+                          color=BrandColors.WHITE,
+                          timestamp=datetime.datetime.now())
+            await ctx.send(embeds=embed)
+        case 4:
+            embed = Embed(title="Single D10 Roll",
+                          description=markdown_strings.header(f"D10: [{SingleRoll.single_d10()}]",2),
+                          color=BrandColors.WHITE,
+                          timestamp=datetime.datetime.now())
+            await ctx.send(embeds=embed)
+        case 5:
+            embed = Embed(title="Single D12 Roll",
+                          description=markdown_strings.header(f"D12: [{SingleRoll.single_d12()}]",2),
+                          color=BrandColors.WHITE,
+                          timestamp=datetime.datetime.now())
+            await ctx.send(embeds=embed)
+        case 6:
+            embed = Embed(title="Single D20 Roll",
+                          description=markdown_strings.header(f"D20: [{SingleRoll.single_d20()}]",2),
+                          color=BrandColors.WHITE,
+                          timestamp=datetime.datetime.now())
+            await ctx.send(embeds=embed)
+        case 7:
+            embed = Embed(title="Single D100 Roll",
+                          description=markdown_strings.header(f"D100: [{SingleRoll.single_d100()}]",2),
+                          color=BrandColors.WHITE,
+                          timestamp=datetime.datetime.now())
+            await ctx.send(embeds=embed)
+        case _:
+            await ctx.send("You didn't select a dice. Aborting...")
 
 bot.start(os.getenv("token"))
